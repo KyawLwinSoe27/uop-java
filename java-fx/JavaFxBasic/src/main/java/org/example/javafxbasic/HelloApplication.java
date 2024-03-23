e6a55fb48d58d0aaad33fad637451d3ed0ed3d70
Kyaw Lwin Soe <>
2024-03-24 03:02:20 +0630

    Install Scene Builder and set up

--- java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
+++ java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
@@ -1,7 +1,9 @@
 package org.example.javafxbasic;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,65 +21,16 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
-       Group root = new Group();
+      try {
+       Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello.fxml")));
        Scene scene = new Scene(root,600,600,Color.LIGHTSKYBLUE);
        Stage stage = new Stage();
 
-        Text text = new Text();
-        text.setText("WHOOOOOOOOOAA!!");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdata",50));
-        text.setFill(Color.LIMEGREEN);
-
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(400);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.RED);
-        line.setOpacity(0.7);
-        line.setRotate(45);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(100);
-        rectangle.setWidth(100);
-        rectangle.setHeight(100);
-        rectangle.setFill(Color.BLUE);
-        rectangle.setStrokeWidth(5);
-        rectangle.setStroke(Color.BLACK);
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(
-                200.0,200.0,
-                300.0,300.0,
-                200.0,300.0
-        );
-        triangle.setFill(Color.YELLOW);
-
-        Circle circle = new Circle();
-        circle.setCenterX(350);
-        circle.setCenterY(350);
-        circle.setRadius(50);
-        circle.setFill(Color.ORANGE);
-
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-test.jpeg")));
-        ImageView imageView = new ImageView(image);
-        imageView.setX(400);
-        imageView.setY(400);
-
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(rectangle);
-        root.getChildren().add(triangle);
-        root.getChildren().add(circle);
-        root.getChildren().add(imageView);
-
-        stage.setScene(scene);
-        stage.show();
+       stage.setScene(scene);
+       stage.show();
+      } catch (Exception e) {
+       e.printStackTrace();
+      }
     }
 
     public static void main(String[] args) {
