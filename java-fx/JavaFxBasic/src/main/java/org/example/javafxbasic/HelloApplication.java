150a4ac0e5117cbc9bcf2d59a51b918cc65152eb
Kyaw Lwin Soe <>
2024-03-24 02:29:10 +0630

    JavaFx Scenes + Drawing on scene

--- java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
+++ java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
@@ -1,31 +1,81 @@
 package org.example.javafxbasic;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
-        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-test.jpeg")));
-        stage.getIcons().add(icon);
+    public void start(Stage primaryStage) {
+       Group root = new Group();
+       Scene scene = new Scene(root,600,600,Color.LIGHTSKYBLUE);
+       Stage stage = new Stage();
 
-        stage.setTitle("Hello!");
-        stage.setResizable(false);
-        stage.setFullScreen(true);
+        Text text = new Text();
+        text.setText("WHOOOOOOOOOAA!!");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Verdata",50));
+        text.setFill(Color.LIMEGREEN);
 
-        stage.setFullScreenExitHint("You ca't escape unless you press q");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(400);
+        line.setEndY(200);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.RED);
+        line.setOpacity(0.7);
+        line.setRotate(45);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(100);
+        rectangle.setY(100);
+        rectangle.setWidth(100);
+        rectangle.setHeight(100);
+        rectangle.setFill(Color.BLUE);
+        rectangle.setStrokeWidth(5);
+        rectangle.setStroke(Color.BLACK);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+                200.0,200.0,
+                300.0,300.0,
+                200.0,300.0
+        );
+        triangle.setFill(Color.YELLOW);
+
+        Circle circle = new Circle();
+        circle.setCenterX(350);
+        circle.setCenterY(350);
+        circle.setRadius(50);
+        circle.setFill(Color.ORANGE);
+
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-test.jpeg")));
+        ImageView imageView = new ImageView(image);
+        imageView.setX(400);
+        imageView.setY(400);
+
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
+        root.getChildren().add(triangle);
+        root.getChildren().add(circle);
+        root.getChildren().add(imageView);
+
         stage.setScene(scene);
         stage.show();
     }
