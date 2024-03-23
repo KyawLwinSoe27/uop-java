16ec3cb3299e79c63485630bc7d02ff6945b6c9f
Kyaw Lwin Soe <>
2024-03-24 04:17:37 +0630

    logout/exit button

--- java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
+++ java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/HelloApplication.java
@@ -1,10 +1,14 @@
 package org.example.javafxbasic;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
@@ -25,16 +29,35 @@ public class HelloApplication extends Application {
        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Scene1.fxml")));
        Scene scene = new Scene(root);
 
-       // multiple css files
           String css = Objects.requireNonNull(this.getClass().getResource("application.css")).toExternalForm();
           scene.getStylesheets().add(css);
-//          scene2.getStylesheets().add(css);
+
 
        stage.setScene(scene);
        stage.show();
+
+       stage.setOnCloseRequest(windowEvent -> {
+           windowEvent.consume();;
+           logout(stage);
+       });
       } catch (Exception e) {
        e.printStackTrace();
       }
+    }
+
+    // exist button and Alt+4
+    public void logout(Stage stage) {
+
+        // confirmation when logout
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you want to save before existing?: ");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("You successfully logged out;");
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
--- java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/Scene2Controller.java
+++ java-fx/JavaFxBasic/src/main/java/org/example/javafxbasic/Scene2Controller.java
@@ -1,13 +1,43 @@
 package org.example.javafxbasic;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Scene2Controller {
     @FXML
     Label nameLabel;
 
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private AnchorPane scenePane;
+
+    Stage stage;
+
     public void displayName(String username) {
         nameLabel.setText("Hello: " + username);
+    }
+
+    public void logout(ActionEvent event) {
+
+        // confirmation when logout
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you want to save before existing?: ");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            System.out.println("You successfully logged out;");
+            stage.close();
+        }
     }
 }
